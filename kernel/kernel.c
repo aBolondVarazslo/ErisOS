55d7aa60665f8da2af0f59006121d3c716d5fb92
aBolondVarazslo <bolondvarazslo@gmail.com>
2025-09-13 15:47:52 +0000

    Minor restructure to remove repeating code

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -12,77 +12,101 @@
 #error "Incorrect compiler. Exiting."
 #endif
 
-/* Hardware text mode color constants. */
-enum vga_color {
-	VGA_COLOR_BLACK = 0,
-	VGA_COLOR_BLUE = 1,
-	VGA_COLOR_GREEN = 2,
-	VGA_COLOR_CYAN = 3,
-	VGA_COLOR_RED = 4,
-	VGA_COLOR_MAGENTA = 5,
-	VGA_COLOR_BROWN = 6,
-	VGA_COLOR_LIGHT_GREY = 7,
-	VGA_COLOR_DARK_GREY = 8,
-	VGA_COLOR_LIGHT_BLUE = 9,
-	VGA_COLOR_LIGHT_GREEN = 10,
-	VGA_COLOR_LIGHT_CYAN = 11,
-	VGA_COLOR_LIGHT_RED = 12,
-	VGA_COLOR_LIGHT_MAGENTA = 13,
-	VGA_COLOR_LIGHT_BROWN = 14,
-	VGA_COLOR_WHITE = 15,
+/* Hardware text mode colour constants. */
+enum vga_colour {
+        VGA_COLOUR_BLACK = 0,
+        VGA_COLOUR_BLUE = 1,
+        VGA_COLOUR_GREEN = 2,
+        VGA_COLOUR_CYAN = 3,
+        VGA_COLOUR_RED = 4,
+        VGA_COLOUR_MAGENTA = 5,
+        VGA_COLOUR_BROWN = 6,
+        VGA_COLOUR_LIGHT_GREY = 7,
+        VGA_COLOUR_DARK_GREY = 8,
+        VGA_COLOUR_LIGHT_BLUE = 9,
+        VGA_COLOUR_LIGHT_GREEN = 10,
+        VGA_COLOUR_LIGHT_CYAN = 11,
+        VGA_COLOUR_LIGHT_RED = 12,
+        VGA_COLOUR_LIGHT_MAGENTA = 13,
+        VGA_COLOUR_LIGHT_BROWN = 14,
+        VGA_COLOUR_WHITE = 15,
 };
 
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
+static inline uint8_t vga_entry_colour(enum vga_colour fg, enum vga_colour bg)
 {
-	return fg | bg << 4;
+        return fg | bg << 4;
 }
 
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color) 
+static inline uint16_t vga_entry(unsigned char uc, uint8_t colour)
 {
-	return (uint16_t) uc | (uint16_t) color << 8;
+        return (uint16_t) uc | (uint16_t) colour << 8;
 }
 
-size_t strlen(const char* str) 
+size_t strlen(const char* str)
 {
-	size_t len = 0;
-	while (str[len])
-		len++;
-	return len;
+        size_t len = 0;
+        while (str[len])
+                len++;
+        return len;
 }
 
 #define VGA_WIDTH   80
 #define VGA_HEIGHT  25
-#define VGA_MEMORY  0xB8000 
+#define VGA_MEMORY  0xB8000
 
 size_t terminal_row;
 size_t terminal_column;
-uint8_t terminal_color;
-uint8_t success_color;
-uint8_t failure_color;
-uint8_t debug_color;
+uint8_t terminal_colour;
+uint8_t success_colour;
+uint8_t failure_colour;
+uint8_t debug_colour;
 uint16_t* terminal_buffer = (uint16_t*)VGA_MEMORY;
 
-void terminal_initialise(void) 
+void terminal_initialise(void)
 {
-	terminal_row = 0;
-	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-	success_color = vga_entry_color(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLACK);	
-	failure_color = vga_entry_color(VGA_COLOR_LIGHT_RED, VGA_COLOR_BLACK);
-	debug_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLUE);
+        terminal_row = 0;
+        terminal_column = 0;
+        terminal_colour = vga_entry_colour(VGA_COLOUR_LIGHT_GREY, VGA_COLOUR_BLACK);
+        success_colour = vga_entry_colour(VGA_COLOUR_LIGHT_GREEN, VGA_COLOUR_BLACK);
+        failure_colour = vga_entry_colour(VGA_COLOUR_LIGHT_RED, VGA_COLOUR_BLACK);
+        debug_colour = vga_entry_colour(VGA_COLOUR_LIGHT_GREY, VGA_COLOUR_BLUE);
 
-	for (size_t y = 0; y < VGA_HEIGHT; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
-			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = vga_entry(' ', terminal_color);
-		}
-	}
+        for (size_t y = 0; y < VGA_HEIGHT; y++) {
+                for (size_t x = 0; x < VGA_WIDTH; x++) {
+                        const size_t index = y * VGA_WIDTH + x;
+                        terminal_buffer[index] = vga_entry(' ', terminal_colour);
+                }
+        }
 }
 
-void terminal_putCharAt(char c, uint8_t color, size_t x, size_t y) 
+uint8_t get_colour(uint8_t status)
 {
-	const size_t index = y * VGA_WIDTH + x;
-	terminal_buffer[index] = vga_entry(c, color);
+        if (status == 0)
+        {
+                status = terminal_colour;
+        }
+
+        else if (status == 1)
+        {
+                status = success_colour;
+        }
+
+        else if (status == 2)
+        {
+                status = failure_colour;
+        }
+
+        else if (status == 3)
+        {
+                status = debug_colour;
+        }
+        return status;
+}
+
+void terminal_putCharAt(char c, uint8_t colour, size_t x, size_t y)
+{
+        const size_t index = y * VGA_WIDTH + x;
+        terminal_buffer[index] = vga_entry(c, colour);
 }
 
 void terminal_typeChar(char c, uint8_t status)
@@ -97,7 +121,7 @@ void terminal_typeChar(char c, uint8_t status)
         else if (c == '\r')
         {
                 terminal_column = 0;
-		
+
         }
 
         else if (c == '\b')
@@ -107,25 +131,15 @@ void terminal_typeChar(char c, uint8_t status)
 
         else if (c == '\t')
         {
-                terminal_column += 4;
+		terminal_column += 4;
         }
 
         else
         {
 
-		uint8_t colour = get_colour(status);
+                uint8_t colour = get_colour(status);
 
-                if (status == 0)
-                        terminal_putCharAt(c, terminal_color, terminal_column, terminal_row);
-
-                else if (status == 1)
-                        terminal_putCharAt(c, success_color, terminal_column, terminal_row);
-
-                else if (status == 2)
-                        terminal_putCharAt(c, failure_color, terminal_column, terminal_row);
-
-                else if (status == 3)
-                        terminal_putCharAt(c, debug_color, terminal_column, terminal_row);
+                terminal_putCharAt(c, colour, terminal_column, terminal_row);
 
                 if (++terminal_column == VGA_WIDTH) {
                         terminal_column = 0;
@@ -135,46 +149,33 @@ void terminal_typeChar(char c, uint8_t status)
         }
 }
 
-void handle_escape(char c)
-{
-
-}
-
-uint8_t get_colour(uint8_t status)
-{
-	if (status == 0)
-	{
-		
-	}
-}
-
 void terminal_write(const char* data, size_t size, uint8_t status)
 {
-	for (size_t i = 0; i < size; i++)
-		terminal_typeChar(data[i], status);
+        for (size_t i = 0; i < size; i++)
+                terminal_typeChar(data[i], status);
 }
 
 void terminal_writeString(const char* data, uint8_t status)
 {
-	terminal_write(data, strlen(data), status);
+        terminal_write(data, strlen(data), status);
 }
 
-void kernel_main(void) 
+void kernel_main(void)
 {
-	/* Initialise terminal interface */
-	terminal_initialise();
+        /* Initialise terminal interface */
+        terminal_initialise();
 
-	terminal_writeString("Kernel boot successful.\n", 1);
-	terminal_writeString("Attempting to load filesystem...\n", 0);
-	terminal_writeString("Filesystem not found!\n", 2);
-	terminal_writeString("Testing...\n", 3);
-	terminal_writeString("Tabs\tend here.\n", 3);
-	terminal_writeString("Backk\bspace.\n", 3);
-	terminal_writeString("Not a\rReturn function.\n", 3);
+        terminal_writeString("Kernel boot successful.\n", 1);
+        terminal_writeString("Attempting to load filesystem...\n", 0);
+        terminal_writeString("Filesystem not found!\n", 2);
+        terminal_writeString("Testing...\n", 3);
+        terminal_writeString("Tabs\tend here.\n", 3);
+        terminal_writeString("Backk\bspace.\n", 3);
+        terminal_writeString("Not a\rReturn function.\n", 3);
 
-	while (1 == 1)
-	{
-		
-	}
+        while (1 == 1)
+        {
+
+        }
 
 }
