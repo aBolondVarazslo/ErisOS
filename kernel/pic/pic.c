14cc31625c4c045f5f96aa992ae5b73340562da2
aBolondVarazslo <abolondvarazslo@gmail.com>
2026-01-27 17:53:49 +0000

    Moved code to an external file for later use, and fixed an error in pic.c

--- kernel/pic/pic.c
+++ kernel/pic/pic.c
@@ -1,28 +1,5 @@
 #include "pic.h"
-#include <stdint.h>
-
-/* Send byte ("value") to I/O port ("port") */
-static inline void outb(uint16_t port, uint8_t value) {
-    /* "outb %0, %1" -> Sends AL register (%0) to port (%1) */
-    /* "=a"(value) -> Puts "value" into the AL register (lower 8 bits of EAX) */
-    /* "Nd"(port) -> Makes port 8-bit immediate or DX register */
-    asm volatile("outb %0, %1" : : "a"(value), "Nd"(port));
-}
-
-/* Read byte from I/O port ("port") */
-static inline uint8_t inb(uint16_t port) {
-    uint8_t ret;
-    /* "inb %1, %0" -> Reads from port (%1) into AL register (%0) */
-    /* "=a"(ret) -> Stores result from AL register into "ret" */
-    /* "Nd"(port) -> Makes port 8-bit immediate or DX register */
-    asm volatile("inb %1, %0" : : "=a"(ret), "Nd"(port));
-}
-
-/* Small delay for older PIC hardware. Ensures the PIC has time to process commands */
-static inline void io_wait(void) {
-    /* Ouput to port 0x80 is used as tiny delay */
-    asm volatile("outb %%al, $0x80" : : "a"(0));
-}
+#include "../io/io.h"
 
 void PIC_remap(uint8_t offset1, uint8_t offset2) {
     /* Start initialisation in cascade mode, and tell PICs ICW4 will be present */
@@ -33,7 +10,7 @@ void PIC_remap(uint8_t offset1, uint8_t offset2) {
     /* Set vector offsets */
     outb(PIC1_DATA, offset1);
     io_wait();
-    outb(PIC2_DATA, offset1);
+    outb(PIC2_DATA, offset2);
     io_wait();
 
     /* ICW3: Tell master/slave about cascade */
