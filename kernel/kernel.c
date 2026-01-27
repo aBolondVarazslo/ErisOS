9034d1929675567db15fb759675378e22b86ed58
aBolondVarazslo <abolondvarazslo@gmail.com>
2026-01-27 18:39:17 +0000

    Add PIC_remap function call to kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,7 @@
 #include "terminal.h"
 #include "idt/idt.h"
 #include "debugging/debugging.h"
+#include "pic/pic.h"
 
 void kernel_main(void) {
     /* Initialise terminal interface */
@@ -10,6 +11,9 @@ void kernel_main(void) {
 
     /* Load IDT */
     idt_init();
+
+    /* Remap PIC */
+    PIC_remap();
 
     terminal_writeString("\nUpdate: 2025/01/27 @ 16:40\n", STATUS_DEBUG);
     terminal_writeString("Reached end of kernel...\n", STATUS_NORMAL);
