bb774b9f5393beecfa973125da4a7c84020d6e93
aBolondVarazslo <abolondvarazslo@gmail.com>
2026-01-27 16:40:28 +0000

    Remove unnecessary kernel code

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,20 +7,11 @@ void kernel_main(void) {
     terminal_initialise();
 
     terminal_writeString("Kernel boot successful.\n", STATUS_SUCCESS);
-    terminal_writeString("Attempting to load filesystem...\n", STATUS_NORMAL);
-    terminal_writeString("Filesystem not found!\n", STATUS_FAILURE);
-    terminal_writeString("Testing...\n\n", STATUS_DEBUG);
 
     /* Load IDT */
     idt_init();
 
-    /* Test IDT and ISR */
-    trigger_debug_exception();
-    trigger_breakpoint();
-
-    // trigger_invalid_opcode();
-
-    terminal_writeString("\nUpdate: 2025/01/24 @ 21:13\n", STATUS_DEBUG);
+    terminal_writeString("\nUpdate: 2025/01/27 @ 16:40\n", STATUS_DEBUG);
 
     while (1);
 }
