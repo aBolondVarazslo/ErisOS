80e8620e2c071256f8514f358da914f5a9c14bef
aBolondVarazslo <abolondvarazslo@gmail.com>
2026-01-28 22:04:12 +0000

    Made irq_stubs array static to fix build error

--- kernel/irq/irq.c
+++ kernel/irq/irq.c
@@ -21,7 +21,7 @@ void irq_common_handler(uint32_t int_no, interrupt_stack_frame_t *frame, uint32_
 }
 
 /* Array of IRQ assembly stubs */
-void (*irq_stubs[16])(void) = {
+static void (*irq_stubs[16])(void) = {
     irq0_stub,  irq1_stub,  irq2_stub,  irq3_stub,
     irq4_stub,  irq5_stub,  irq6_stub,  irq7_stub,
     irq8_stub,  irq9_stub,  irq10_stub, irq11_stub,
