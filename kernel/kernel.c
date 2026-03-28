59903516fb491e7fdf742a6fe56cff438aa2dc90
aBolondVarazslo <abolondvarazslo@gmail.com>
2026-03-28 16:38:05 +0000

    Remove manually assigning IRQ0 in kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -21,22 +21,13 @@ void kernel_main(void) {
     /* Load IDT */
     idt_init();
 
-    uint16_t cs16;
-    asm volatile("mov %%cs, %0" : "=r"(cs16));
-    set_idt_entry(0x20, (uint32_t)irq0_stub, cs16, IDT_FLAG_KERNEL_INTERRUPT);
-    terminal_writeString("Forced IRQ stub\n", STATUS_DEBUG);
-
-    terminal_writeString("irq0_stub address = ", STATUS_DEBUG);
-    terminal_writeHex((uint32_t)irq0_stub);
-    terminal_writeString("\n", STATUS_DEBUG);
-
     /* Remap PIC */
     PIC_remap(0x20, 0x28);
     PIC_set_mask(0xFE, 0xFF);
 
     /* Initialise PIT (IRQ0) */
     pit_init(100);
-    //asm volatile("sti");
+    asm volatile("sti");
     terminal_writeString("Interrupts Enabled\n", STATUS_SUCCESS);
 
     terminal_writeString("\nUpdate: 2025/03/24 @ 20:36\n", STATUS_DEBUG);
