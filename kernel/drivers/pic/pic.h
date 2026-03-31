0ff22ed4d5bd4171ddeda9631e036b9e29561296
aBolondVarazslo <abolondvarazslo@gmail.com>
2026-03-31 13:11:13 +0100

    Simplify PIC definitions

--- kernel/drivers/pic/pic.h
+++ kernel/drivers/pic/pic.h
@@ -5,13 +5,10 @@
 
 /* SOURCE: https://wiki.osdev.org/8259_PIC#Initialisation */
 
-#define PIC1    0x20    /* IO base address for master PIC */
-#define PIC2    0xA0    /* IO base address for slave PIC */
-
-#define PIC1_COMMAND    PIC1
-#define PIC1_DATA   (PIC1+1)
-#define PIC2_COMMAND    PIC2
-#define PIC2_DATA   (PIC2+1)
+#define PIC1_COMMAND    0x20    /* IO base address for master PIC */
+#define PIC2_COMMAND    0xA0    /* IO base address for slave PIC */
+#define PIC1_DATA   (PIC1_COMMAND+1)
+#define PIC2_DATA   (PIC2_COMMAND+1)
 
 #define PIC_EOI 0x20    /* End-of-Interrupt command code */
 
