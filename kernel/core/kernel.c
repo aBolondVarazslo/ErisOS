0285e50fae7d8b8ec9e1b60533b05283d79166a1
aBolondVarazslo <abolondvarazslo@gmail.com>
2026-03-31 12:35:09 +0100

    Removed output message for IRQ0

--- kernel/core/kernel.c
+++ kernel/core/kernel.c
@@ -30,7 +30,7 @@ void kernel_main(void) {
     asm volatile("sti");
     terminal_writeString("Interrupts Enabled\n", STATUS_SUCCESS);
 
-    terminal_writeString("\nUpdate: 2025/03/24 @ 20:36\n", STATUS_DEBUG);
+    terminal_writeString("\nUpdate: 2025/03/31 @ 12:34\n", STATUS_DEBUG);
     terminal_writeString("Reached end of kernel...\n", STATUS_NORMAL);
 
     while (1);
--- kernel/cpu/irq/irq.c
+++ kernel/cpu/irq/irq.c
@@ -3,11 +3,13 @@
 #include "../../lib/terminal.h"
 
 void irq_common_handler(uint32_t int_no) {
-    uint32_t irq = int_no - 0x20;   /* Convert vector number to IRQ number */
+    uint32_t irq = int_no - 0x20; /* Convert vector number to IRQ number */
 
-    terminal_writeString("IRQ ", STATUS_DEBUG);
-    terminal_writeHex(irq);
-    terminal_writeString(" fired\n", STATUS_DEBUG);
+    if (irq != 0) {
+        terminal_writeString("IRQ ", STATUS_DEBUG);
+        terminal_writeHex(irq);
+        terminal_writeString(" fired\n", STATUS_DEBUG);
+    }
 
     /* Send End Of Interrupt */
     if (irq >= 8) {
@@ -17,9 +19,6 @@ void irq_common_handler(uint32_t int_no) {
 }
 
 /* Array of IRQ assembly stubs */
-void (*irq_stubs[16])(void) = {
-    irq0_stub,  irq1_stub,  irq2_stub,  irq3_stub,
-    irq4_stub,  irq5_stub,  irq6_stub,  irq7_stub,
-    irq8_stub,  irq9_stub,  irq10_stub, irq11_stub,
-    irq12_stub, irq13_stub, irq14_stub, irq15_stub
-};
+void (*irq_stubs[16])(void) = {irq0_stub,  irq1_stub,  irq2_stub,  irq3_stub, irq4_stub,  irq5_stub,
+                               irq6_stub,  irq7_stub,  irq8_stub,  irq9_stub, irq10_stub, irq11_stub,
+                               irq12_stub, irq13_stub, irq14_stub, irq15_stub};
