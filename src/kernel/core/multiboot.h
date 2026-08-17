df5f949970e4c4efe960c42c058b0f6ded5cebc2
aBolondVarazslo <abolondvarazslo@gmail.com>
2026-08-17 15:25:32 +0100

    Added memory mapping

---
+++ src/kernel/core/multiboot.h
@@ -0,0 +1,32 @@
+#ifndef MULTIBOOT_H
+#define MULTIBOOT_H
+
+#include <stdint.h>
+
+typedef struct {
+    uint32_t flags;
+
+    uint32_t mem_lower;
+    uint32_t mem_higher;
+
+    uint32_t boot_device;
+    uint32_t cmdline;
+
+    uint32_t mods_count;
+    uint32_t mods_addr;
+
+    uint32_t syms[4];
+
+    uint32_t mmap_length;
+    uint32_t mmap_addr;
+} multiboot_info_t;
+
+typedef struct {
+    uint32_t size;
+    uint64_t base_addr;
+    uint64_t length;
+    uint32_t type;
+} __attribute__((packed)) multiboot_mmap_entry_t;
+
+
+#endif
