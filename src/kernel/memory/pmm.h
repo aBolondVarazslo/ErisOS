a4c122b942a6841b7b47aadd76fe54b1e9f127c6
aBolondVarazslo <abolondvarazslo@gmail.com>
2026-08-29 23:33:34 +0100

    Added pmm_alloc_frame() and pmm_free_frame()

--- src/kernel/memory/pmm.h
+++ src/kernel/memory/pmm.h
@@ -6,10 +6,13 @@
 
 #define FRAME_SIZE 4096
 #define MAX_FRAMES 262144 /* 1 GB of RAM */
+#define PMM_INVALID_FRAME 0xFFFFFFFF
 
 void pmm_init(multiboot_info_t *mb_info);
 uint32_t pmm_alloc_frame(void);
 void pmm_free_frame(uint32_t frame_addr);
 uint32_t pmm_get_free_frame_count(void);
+uint32_t pmm_alloc_frame(void);
+void pmm_free_frame(uint32_t frame_addr);
 
 #endif
